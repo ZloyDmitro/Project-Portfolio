ecc9f8cf37a274b4cfc06ce6306d99bf63afb3e3
ZloyDmitro <zloydmitro@gmail.com>
2024-05-15 11:49:06 +0200

    some changes to Html files

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables from .env file
 dotenv_path = BASE_DIR / '.env'
-dotenv.load_dotenv(dotenv_path)
+dotenv.read_dotenv(dotenv_path)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -32,7 +32,7 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,14 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static",]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-# Media settings
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+# # Media settings
+# MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
